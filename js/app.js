2d874e8ca21d9925cb8659f65c98b857dfff4ce1
Jorge Arévalo <jorgeas80@gmail.com>
2015-10-15 01:11:17 +0200

    Making Angular + Restangular work against my heroku app. Still need to configure CORS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 // MODULE
-var ntApp = angular.module('ntApp', []);
+var ntApp = angular.module('ntApp', ['restangular']);
+
+// Restangular service that uses Heroku
+/*
+ntApp.factory('HerokuRestangular', function(Restangular) {
+  return Restangular.withConfig(function(RestangularConfigurer) {
+    RestangularConfigurer.setBaseUrl('http://cercanias-api.herokuapp.com/');
+  });
+});
+*/
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
-    
-}]);
+ntApp.controller('mainController', function ($scope, Restangular) {
+    Restangular.setBaseUrl('http://cercanias-api.herokuapp.com/');
+    $scope.cities = Restangular.all('city', {"format": "json"}).getList().$object;
+});
 
 
 
